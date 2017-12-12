013861bb6c6a343da5a740b3e92025644d7d2c3d
Kirill Skorobogatov <Kirill9806@gmail.com>
2017-12-12 16:28:22 +0300

    Update task_url.py

--- task_url.py
+++ task_url.py
@@ -1,12 +1,20 @@
+#link: https://api.vk.com/method/users.get?user_ids=100,777,dm,skorking&v=5.8&fields=nickname,sex,status,education,online,last_seen
+
 import urllib.request
 import json, pprint
 
-html = urllib.request.urlopen("https://api.vk.com/method/users.get?user_ids=100,777,123321,12021&v=5.8&fields=status,%20education,%20last_seen").read()
+html = urllib.request.urlopen("http://bit.ly/2AvZeGj").read()
 data = json.loads(html)
 s = data.get('response')
 #pprint.pprint(s)
-for user in data:
+
+print(' ' + '-*-*'*12 + '\n')
+for user in s:
+    print(" First name: " + str(user['first_name']))
+    print(" Last name: " + str(user['last_name']))
+    print(" Nickname: " + str(user['nickname']))
     print(" Status: " + str(user['status']))
-    print(" Education: " + str(user['education']))
+    print(" Online: " + str(user['online']))
+    print(" Sex: " + str(user['sex']))
     print(" Last_seen: " + str(user['last_seen'])+ '\n')
-    print(' ' + '-*-*'*16 + '\n')
+    print(' ' + '-*-*'*12 + '\n')
